b3668e99c3778ef6c7acaa857378cf2d17a49335
Jonathan Zempel <jzempel@gmail.com>
2016-01-19 15:29:45 -0800

    [#2] Add .c-avatar--borderless class (and demo)

---
+++ packages/css-avatars/demo/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.js-dark').click(function() {
+    $('html').toggleClass('u-bg-dark-gray u-fg-white');
+    $('.c-avatar').toggleClass('c-avatar--borderless');
+  });
+});
