3e7a07295e8d35fdaf8914e9b98e40abec8761f7
Jonathan Zempel <jzempel@gmail.com>
2016-04-08 16:10:32 -0700

    Initial tab state updates

--- packages/css-tabs/demo/index.js
+++ packages/css-tabs/demo/index.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $('.c-tab').toggleClass('is-rtl');
     $('.js-rtl-class').toggleClass('u-display-none');
   });
+
+  $('.c-tab__list__item').mouseup(function() {
+    $(this).blur();
+  });
 });
