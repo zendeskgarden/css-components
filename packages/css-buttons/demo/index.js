a62e502f94bcc8370c379fb0042674b3b45a62b1
Jonathan Zempel <jzempel@gmail.com>
2016-10-27 11:38:32 -0700

    Merge branch 'jzempel/dark' into master

--- packages/css-buttons/demo/index.js
+++ packages/css-buttons/demo/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $('.js-dark').click(function() {
+    $('html').toggleClass('u-bg-algae u-fg-white');
+    $('.c-btn').toggleClass('c-btn--dark');
+  });
+
   $('.c-btn').focus(function() {
     $(this).addClass('is-focused');
   }).blur(function() {
