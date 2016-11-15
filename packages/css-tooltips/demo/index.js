5c92964ef17e0c645451ab9748ad7108a4ddf355
Jonathan Zempel <jzempel@gmail.com>
2016-11-15 10:11:09 -0800

    [#3] Add .c-tooltip--dark styling & demo

---
+++ packages/css-tooltips/demo/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.js-dark').click(function() {
+    $('html').toggleClass('u-bg-daintree u-fg-white');
+    $('.c-tooltip').toggleClass('c-tooltip--dark');
+    $('.u-fg-oil').toggleClass('u-fg-white');
+  });
+});
