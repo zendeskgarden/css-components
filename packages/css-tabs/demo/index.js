207633bf7acdb91094f2af890a356a26bd82a620
Jonathan Zempel <jzempel@gmail.com>
2016-11-28 14:36:03 -0800

    Initial electroid styling

--- packages/css-tabs/demo/index.js
+++ packages/css-tabs/demo/index.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
     $('.js-rtl-class').toggleClass('u-display-none');
   });
 
+  $('.c-tab__list__item').click(function() {
+    var $parent = $(this).parent('.js-tablist');
+
+    if ($parent.length) {
+      $parent.children('.c-tab__list__item').removeClass('is-selected');
+      $(this).addClass('is-selected');
+    }
+  });
+
   $('.c-tab__list__item').mouseup(function() {
     $(this).blur();
   });
