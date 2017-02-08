666e3ffe5e20daaca9f9d5e29e0838d42c30f36b
Jonathan Zempel <jzempel@gmail.com>
2017-02-08 13:03:32 -0800

    [#5] Update demo page

--- packages/css-tooltips/demo/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.js-dark').click(function() {
-    $('html').toggleClass('u-bg-daintree u-fg-white');
-    $('.c-tooltip').toggleClass('c-tooltip--dark');
-    $('.u-fg-oil').toggleClass('u-fg-white');
-  });
-});
