64aa721277aa88587a6f66758a72dded68713e2f
Jonathan Zempel <jzempel@gmail.com>
2017-09-08 05:57:53 -0700

    [#1] Initial dark mode; re-work overflow & sortable

--- packages/css-tables/demo/index.js
+++ packages/css-tables/demo/index.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-  var $focus = null;
-
-  $(document).on('blur', '.c-table__row', function() {
-    if ($focus !== null) {
-      $(this).toggleClass('is-focused', $(this).get(0) === $focus.get(0));
-    }
-  });
+  var mouse = false;
 
   $(document).on('click', '.c-table__row--header .c-chk__input', function() {
     var $rows = $(this).parents('.c-table').find('.c-table__row:not(.c-table__row--header):not(.c-table__row--group)');
@@ -19,11 +13,36 @@ $(document).ready(function() {
     $(this).parents('.c-table__row').toggleClass('is-selected');
   });
 
-  $(document).on('focus', '.c-table__row', function() {
-    $(this).siblings().removeClass('.is-focused');
+  $(document).on('focusin', '.c-table__row', function() {
+    $(this).addClass('is-focused');
+  }).on('blur', '.c-table__row', function() {
+    $(this).removeClass('is-focused');
   });
 
-  $(document).on('mousedown', '.c-chk', function() {
-    $focus = $(this).parents('.c-table__row');
+  $(document).on('click', '.c-table__row__cell__sortable', function() {
+    var $this = $(this);
+
+    if ($this.is('.is-asc')) {
+      $this.removeClass('is-asc').addClass('is-desc');
+    } else if ($this.is('.is-desc')) {
+      $this.removeClass('is-desc').addClass('is-asc');
+    } else {
+      $this.addClass('is-asc');
+    }
+
+    $this.blur();
+
+    return false;
+  }).on('mousedown', '.c-table__row__cell__sortable', function() {
+    mouse = true;
+    $(this).removeClass('is-focused');
+  }).on('focus', '.c-table__row__cell__sortable', function() {
+    if (mouse) {
+      mouse = false;
+    } else {
+      $(this).addClass('is-focused');
+    }
+  }).on('blur', '.c-table__row__cell__sortable', function() {
+    $(this).removeClass('is-focused');
   });
 });
