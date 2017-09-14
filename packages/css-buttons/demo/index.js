be804bc8f97251c270822de91d72436261389945
Jonathan Zempel <jzempel@gmail.com>
2017-09-14 14:38:03 -0700

    Merge branch 'jzempel/custom' into master

--- packages/css-buttons/demo/index.js
+++ packages/css-buttons/demo/index.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-  $('.js-dark').click(function() {
-    $('html').toggleClass('u-bg-algae u-fg-white');
-    $('.c-btn').toggleClass('c-btn--dark');
-  });
-
-  $('.c-btn').focus(function() {
-    $(this).addClass('is-focused');
-  }).blur(function() {
-    $(this).removeClass('is-focused');
-  }).mouseup(function() {
-    $(this).removeClass('is-focused');
+  $('.js-custom').click(function() {
+    $('.c-btn:not(.c-playground .c-btn)').toggleClass('c-btn--custom');
   });
 });
