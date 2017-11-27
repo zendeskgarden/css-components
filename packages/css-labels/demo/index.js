1b0b79b4ed88610968abe1a5c984b3194844714c
Jonathan Zempel <jzempel@gmail.com>
2017-11-27 14:51:59 -0800

    Update demo with custom CSS

--- packages/css-labels/demo/index.js
+++ packages/css-labels/demo/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  $('.js-custom').click(function() {
+    $('.c-label:not(.c-playground .c-label)').toggleClass('c-label--custom');
+  });
+
   $('.js-resize').on('input', function() {
     $('.js-txt').css('width', $(this).val() + '%');
   });
