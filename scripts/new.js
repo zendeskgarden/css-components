a8b6955666b84bf19cde5269e0a3f86fc19432f4
Jonathan Zempel <jzempel@gmail.com>
2017-12-23 15:13:47 -0800

    Update new component script

--- scripts/new.js
+++ scripts/new.js
@@ -4,47 +4,63 @@ const fs = require('fs');
 const klaw = require('klaw-sync');
 const ncp = require('ncp').ncp;
 const path = require('path');
+const rimraf = require('rimraf');
 
 Handlebars.registerHelper('capitalize', value => {
   return value.charAt(0).toUpperCase() + value.slice(1);
 });
 
+const parent = path.join(__dirname, '..');
+const packages = path.join(parent, 'packages');
+
+function linkCss(name) {
+}
+
+function updateDemo(name) {
+  const source = path.join(packages, name, 'demo');
+  const destination = path.join(parent, 'demo', name);
+
+  ncp(source, destination, error => {
+    if (error) {
+      console.log(chalk.red('error'), error);
+    } else {
+      rimraf(source, () => {
+        console.log(chalk.green('success'), 'Updated component demo');
+      });
+    }
+  });
+}
+
 function addComponent(name) {
-  const packages = path.join(__dirname, '..', 'packages');
   const source = path.join(packages, '.template');
   const destination = path.join(packages, name);
 
-  if (!fs.existsSync(destination)) {
+  if (fs.existsSync(destination)) {
+    console.log(chalk.red('error'), `Component package exists`);
+  } else {
     ncp(source, destination, error => {
       if (error) {
         console.log(chalk.red('error'), error);
-        process.exit();
+      } else {
+        const items = klaw(destination, { nodir: true });
+
+        items.forEach(item => {
+          const string = fs.readFileSync(item.path, 'utf8');
+          const template = Handlebars.compile(string);
+          const data = template({ component: name });
+
+          fs.writeFileSync(item.path, data, 'utf8');
+        });
+
+        console.log(chalk.green('success'), 'Added new component');
+        updateDemo(name);
       }
-
-      const items = klaw(destination, {
-        filter: item => !item.stats.isSymbolicLink(),
-        nodir: true
-      });
-
-      items.forEach(item => {
-        const string = fs.readFileSync(item.path, 'utf8');
-        const template = Handlebars.compile(string);
-        const data = template({ component: name });
-
-        fs.writeFileSync(item.path, data, 'utf8');
-      });
     });
-  } else {
-    console.log(chalk.red('error'), `Component package exists`);
-    process.exit();
   }
 }
 
 if (process.argv.length >= 3) {
-  const component = process.argv[2];
-
-  addComponent(component);
+  addComponent(process.argv[2])
 } else {
   console.log(chalk.red('error'), 'Missing component name');
-  process.exit();
 }
