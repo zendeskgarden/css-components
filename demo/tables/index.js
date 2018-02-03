bb37f618e73c0c300163230a4bec8c5cdfebad56
Jonathan Zempel <jzempel@gmail.com>
2018-02-03 09:50:24 -0800

    feat: add css-tables package to monorepo (#17)

---
+++ demo/tables/index.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  Garden.customClasses.push('.c-table');
+  Garden.darkClasses.push('.c-table');
+  Garden.rtlClasses.push('.c-table');
+
+  $(document).on('click', '.c-table__row--header .c-chk__input', function() {
+    var $rows = $(this).parents('.c-table').find('.c-table__row:not(.c-table__row--header):not(.c-table__row--group)');
+    var isSelected = $(this).is(':checked');
+
+    $rows.toggleClass('is-selected', isSelected);
+    $rows.find('.c-chk__input').prop('checked', isSelected);
+  });
+
+  $(document).on('click', '.c-table__row:not(.c-table__row--header) .c-chk__input', function() {
+    $(this).parents('.c-table__row').toggleClass('is-selected');
+  });
+
+  $(document).on('focusin', '.c-table__row', function() {
+    $(this).addClass('is-focused');
+  }).on('blur', '.c-table__row', function() {
+    $(this).removeClass('is-focused');
+  });
+
+  var mouse = false;
+
+  $(document).on('focus', '.c-table__row__cell__overflow', function() {
+    if (mouse) {
+      mouse = false;
+    } else {
+      $(this).addClass('is-focused');
+    }
+  }).on('mousedown', '.c-table__row__cell__overflow', function() {
+    mouse = true;
+    $(this).removeClass('is-focused');
+  }).on('blur', '.c-table__row__cell__overflow', function() {
+    $(this).removeClass('is-focused');
+  });
+
+  $(document).on('click', '.c-table__row__cell__sortable', function() {
+    var $this = $(this);
+
+    if ($this.is('.is-ascending')) {
+      $this.removeClass('is-ascending').addClass('is-descending');
+    } else if ($this.is('.is-descending')) {
+      $this.removeClass('is-descending').addClass('is-ascending');
+    } else {
+      $this.addClass('is-ascending');
+    }
+  }).on('mousedown', '.c-table__row__cell__sortable', function() {
+    mouse = true;
+    $(this).removeClass('is-focused');
+  }).on('focus', '.c-table__row__cell__sortable', function() {
+    if (mouse) {
+      mouse = false;
+      $(this).blur();
+    } else {
+      $(this).addClass('is-focused');
+    }
+  }).on('blur', '.c-table__row__cell__sortable', function() {
+    $(this).removeClass('is-focused');
+  });
+
+  $('.js-display').change(function() {
+    var value = $(this).val();
+    var $table = $('.c-table:not(.c-playground .c-table)');
+
+    $table.removeClass('c-table--sm c-table--lg');
+
+    if (value.length > 0) {
+      $table.addClass('c-table--' + value);
+    }
+  });
+
+  $('.js-stripes').change(function() {
+    if ($(this).is(':checked')) {
+      $('.c-table:not(.c-playground .c-table)').each(function() {
+        var addStripe = true;
+
+        $('.c-table__row', $(this)).each(function() {
+          var $row = $(this);
+
+          if ($row.hasClass('c-table__row--header')) {
+            addStripe = true;
+          } else if ($row.hasClass('c-table__row--group')) {
+            addStripe = false;
+          } else {
+            $row.toggleClass('c-table__row--stripe', addStripe);
+            addStripe = !addStripe;
+          }
+        });
+      });
+    } else {
+      $('.c-table__row').removeClass('c-table__row--stripe');
+    }
+  });
+});
---
+++ packages/tables/CHANGELOG.md
@@ -0,0 +1,42 @@
+# Change Log
+
+<a name="1.0.5"></a>
+## [1.0.5] - 2018-02-02
+### Fixed (per [#10](https://github.com/zendeskgarden/css-tables/pull/10))
+- Handle truncation on `.c-table__row__cell--truncate
+  .c-table__row__cell__sortable` elements.
+
+## [1.0.4] - 2017-12-11
+### Fixed (per [#9](https://github.com/zendeskgarden/css-tables/pull/9))
+- Add box sizing styling overrides to `.c-table__row` and
+  `.c-table__row__cell`.
+
+## [1.0.3] - 2017-12-08
+### Fixed (per [#8](https://github.com/zendeskgarden/css-tables/pull/8))
+- Add button element styling resets to `.c-table__row__cell__sortable`.
+
+## [1.0.2] - 2017-10-12
+### Fixed (per [#5](https://github.com/zendeskgarden/css-tables/pull/5))
+- Increase speed of row hover transition.
+
+## [1.0.1] - 2017-09-28
+### Fixed (per [#4](https://github.com/zendeskgarden/css-tables/pull/4))
+- Move border styling from cells back to rows.
+
+## [1.0.0] - 2017-09-19
+### Added (per [#3](https://github.com/zendeskgarden/css-tables/issues/3))
+- Include complementary `.c-table--custom` CSS demonstrating how to apply
+  a primary color change.
+
+### Fixed (per [#2](https://github.com/zendeskgarden/css-tables/pull/2))
+- Apply state changes to row `border-bottom` only to fix rendering for
+  IE11.
+
+## 0.1.0 - 2017-09-13
+
+[1.0.5]: https://github.com/zendeskgarden/css-tables/compare/v1.0.4...v1.0.5
+[1.0.4]: https://github.com/zendeskgarden/css-tables/compare/v1.0.3...v1.0.4
+[1.0.3]: https://github.com/zendeskgarden/css-tables/compare/v1.0.2...v1.0.3
+[1.0.2]: https://github.com/zendeskgarden/css-tables/compare/v1.0.1...v1.0.2
+[1.0.1]: https://github.com/zendeskgarden/css-tables/compare/v1.0.0...v1.0.1
+[1.0.0]: https://github.com/zendeskgarden/css-tables/compare/v0.1.0...v1.0.0
