41f146b344a46b3f19246f1aada3bc17ff1fddfb
Jonathan Zempel <jzempel@gmail.com>
2018-03-09 09:10:40 -0800

    chore: repair gh-pages deploy (#41)

---
+++ scripts/gh-pages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const ghPages = require('gh-pages');
+
+ghPages.publish('demo', {
+  repo: `https://${process.env.GITHUB_TOKEN}@github.com/zendeskgarden/css-components.git`,
+  silent: true
+});
