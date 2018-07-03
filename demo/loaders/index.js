9ce581e5ffd3fbc7e17168c5c93df55df6b662eb
Jonathan Zempel <jzempel@gmail.com>
2018-07-02 19:26:12 -0700

    Refactor loaders component

--- demo/loaders/index.js
+++ demo/loaders/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.js-animate').click(function() {
     const isChecked = $(this).is(':checked');
 
-    $('.js-dots').toggleClass('o-loader-dots', isChecked);
+    $('.js-dots').toggleClass('o-loader--dots', isChecked);
 
     if (isChecked) {
       lottie.play();
@@ -15,7 +15,7 @@ $(document).ready(function() {
     const value = $(this).val();
 
     if (value) {
-      $('.o-loader-dots').css('color', value);
+      $('.o-loader--dots').css('color', value);
     }
   });
 
@@ -23,10 +23,7 @@ $(document).ready(function() {
     const size = $(this).val();
 
     $('.js-lottie').css('width', size);
-    $('.js-dots').css({
-      width: size,
-      height: size * .9
-    });
+    $('.js-dots').css('fontSize', size);
   }).click(function() {
     return false;
   });
@@ -52,84 +49,89 @@ $(document).ready(function() {
   const KEYFRAMES = {};
   const LOOP = true;
   const AUTOPLAY = false;
-  const PATH = 'dots.json';
+  const PATH = 'bodymovin/dots.json';
   const SUBFRAME = false;
   const SPEED = 1;
-  const animation = lottie.loadAnimation({
-    container: $('.js-lottie')[0],
-    loop: LOOP,
-    autoplay: AUTOPLAY,
-    path: PATH
-  });
-  const initData = (name, x, y) => {
-    if (!(name in KEYFRAMES)) {
-      KEYFRAMES[name] = {};
-      KEYFRAMES[name].X = 0;
-      KEYFRAMES[name].Y = 0;
-    }
 
-    if (KEYFRAMES[name].X === 0) {
-      KEYFRAMES[name].X = x;
-    }
-
-    if (KEYFRAMES[name].Y === 0) {
-      if (name === 'yellow' && y !== 0) {
-        // Compensate for lower starting dot.
-        y -= 5;
-      }
-
-      KEYFRAMES[name].Y = y;
-    }
-  }
-  const addTransform = (name, key, x, y) => {
-    initData(name, x, y);
-
-    x -= KEYFRAMES[name].X;
-    y -= KEYFRAMES[name].Y;
-
-    if (x !== 0 || y !== 0) {
-      x = Math.round(x);
-      y = Math.round(y);
-
-      if (y === 0) {
-        KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px); }`;
-      } else {
-        KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px, ${y}px); }`;
-      }
-    }
-  }
-
-  animation.setSubframe(SUBFRAME);
-  animation.setSpeed(SPEED);
-
-  animation.onEnterFrame = event => {
-    const elements = animation.renderer.elements;
-
-    elements.forEach(element => {
-      const matrix = element.finalTransform.mat.to2dCSS();
-      const [x, y] = matrix.match(PATTERN_MATRIX).slice(1);
-
-      addTransform(element.data.nm.toLowerCase(), event.currentTime, parseFloat(x), parseFloat(y));
+  if ($('.js-lottie').length) {
+    const animation = lottie.loadAnimation({
+      container: $('.js-lottie')[0],
+      loop: LOOP,
+      autoplay: AUTOPLAY,
+      path: PATH
     });
-  }
+    const initData = (name, x, y) => {
+      if (!(name in KEYFRAMES)) {
+        KEYFRAMES[name] = {};
+        KEYFRAMES[name].X = 0;
+        KEYFRAMES[name].Y = 0;
+      }
 
-  animation.onLoopComplete = (event) => {
-    if (event.currentLoop === 1) {
-      Object.keys(KEYFRAMES).forEach(name => {
-        let keyframes = `@keyframes zd-${name} {`;
-        let keys = Object.keys(KEYFRAMES[name]).filter(key => (key !== 'X' && key !== 'Y'));
+      if (KEYFRAMES[name].X === 0) {
+        KEYFRAMES[name].X = x;
+      }
 
-        keys.forEach(key => {
-          keyframes = `${keyframes}\n  ${KEYFRAMES[name][key]}`;
-        });
+      if (KEYFRAMES[name].Y === 0) {
+        if (name === 'yellow' && y !== 0) {
+          // Compensate for lower starting dot.
+          y -= 5;
+        }
 
-        keyframes = `${keyframes}\n}`;
-        console.log(keyframes);
+        KEYFRAMES[name].Y = y;
+      }
+    }
+    const addTransform = (name, key, x, y) => {
+      initData(name, x, y);
+
+      x -= KEYFRAMES[name].X;
+      y -= KEYFRAMES[name].Y;
+
+      if (x !== 0 || y !== 0) {
+        x = Math.round(x);
+        y = Math.round(y);
+
+        if (y === 0) {
+          KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px); }`;
+        } else {
+          KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px, ${y}px); }`;
+        }
+      }
+    }
+
+    animation.setSubframe(SUBFRAME);
+    animation.setSpeed(SPEED);
+
+    animation.onEnterFrame = event => {
+      const elements = animation.renderer.elements;
+
+      elements.forEach(element => {
+        const matrix = element.finalTransform.mat.to2dCSS();
+        const [x, y] = matrix.match(PATTERN_MATRIX).slice(1);
+
+        addTransform(element.data.nm.toLowerCase(), event.currentTime, parseFloat(x), parseFloat(y));
       });
     }
-  }
 
-  animation.addEventListener('DOMLoaded', () => {
+    animation.onLoopComplete = (event) => {
+      if (event.currentLoop === 1) {
+        Object.keys(KEYFRAMES).forEach(name => {
+          let keyframes = `@keyframes zd-${name} {`;
+          let keys = Object.keys(KEYFRAMES[name]).filter(key => (key !== 'X' && key !== 'Y'));
+
+          keys.forEach(key => {
+            keyframes = `${keyframes}\n  ${KEYFRAMES[name][key]}`;
+          });
+
+          keyframes = `${keyframes}\n}`;
+          console.log(keyframes);
+        });
+      }
+    }
+
+    animation.addEventListener('DOMLoaded', () => {
+      $('.js-animate').triggerHandler('click');
+    });
+  } else {
     $('.js-animate').triggerHandler('click');
-  });
+  }
 });
