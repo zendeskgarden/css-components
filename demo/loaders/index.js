be1794d4818d1de95fbe1339c3d2a171400a5d7e
Jonathan Zempel <jzempel@gmail.com>
2018-08-02 12:12:05 -0700

    Refine spinner animation

--- demo/loaders/index.js
+++ demo/loaders/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $('.js-animate').click(function() {
     const isChecked = $(this).is(':checked');
 
@@ -47,130 +48,129 @@ $(document).ready(function() {
   });
 
   const PATTERN_MATRIX = /matrix\([-+]?\d*\.?\d+,[-+]?\d*\.?\d+,[-+]?\d*\.?\d+,[-+]?\d*\.?\d+,([-+]?\d*\.?\d+),([-+]?\d*\.?\d+)\)/;
-  const PATTERN_PATH = /C*.([-+]?\d*\.?\d*),([-+]?\d*\.?\d*)$/;
+  const PATTERN_STARTPOINT = /M([-+]?\d*\.?\d*),([-+]?\d*\.?\d*)/;
   const KEYFRAMES = {};
   const LOOP = true;
   const AUTOPLAY = false;
-  const PATH = 'bodymovin/spinner.json';
   const SUBFRAME = false;
   const SPEED = 1;
 
   if ($('.js-lottie').length) {
-    const animation = lottie.loadAnimation({
-      container: $('.js-lottie')[0],
-      loop: LOOP,
-      autoplay: AUTOPLAY,
-      path: PATH
-    });
-    const initData = (name, x, y) => {
-      if (!(name in KEYFRAMES)) {
-        KEYFRAMES[name] = {};
-        KEYFRAMES[name].X = 0;
-        KEYFRAMES[name].Y = 0;
-      }
-
-      if (KEYFRAMES[name].X === 0) {
-        KEYFRAMES[name].X = x;
-      }
-
-      if (KEYFRAMES[name].Y === 0) {
-        if (name === 'yellow' && y !== 0) {
-          // Compensate for lower starting dot.
-          y -= 5;
+    $('.js-lottie').each(function() {
+      const animation = lottie.loadAnimation({
+        container: $(this)[0],
+        loop: LOOP,
+        autoplay: AUTOPLAY,
+        path: $(this).data('path')
+      });
+      const initData = (name, x, y) => {
+        if (!(name in KEYFRAMES)) {
+          KEYFRAMES[name] = {};
+          KEYFRAMES[name].X = 0;
+          KEYFRAMES[name].Y = 0;
         }
 
-        KEYFRAMES[name].Y = y;
-      }
-    };
-    const addTransform = (name, key, x, y) => {
-      initData(name, x, y);
-
-      x -= KEYFRAMES[name].X;
-      y -= KEYFRAMES[name].Y;
-
-      if (x !== 0 || y !== 0) {
-        x = Math.round(x);
-        y = Math.round(y);
-
-        if (y === 0) {
-          KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px); }`;
-        } else {
-          KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px, ${y}px); }`;
+        if (KEYFRAMES[name].X === 0) {
+          KEYFRAMES[name].X = x;
         }
-      }
-    };
-    const addDash = (name, key, rotate, length, x, y) => {
-      initData(name, 0, 0);
-      rotate = Math.round(rotate - 90);
-      length = Math.round(length / 7.5);
 
-      KEYFRAMES[name][key] = `${key}% { stroke-dasharray: ${length} 250; transform: rotate(${rotate}deg); }`;
-    };
-    const toDegrees = (x, y) => {
-      const radians = Math.atan2(y, x);
-      const degrees = radians * (180 / Math.PI);
-      let retVal = degrees + 90;
+        if (KEYFRAMES[name].Y === 0) {
+          if (name === 'yellow' && y !== 0) {
+            // Compensate for lower starting dot.
+            y -= 5;
+          }
 
-      if (x < 0 && y < 0) {
-        retVal += 360;
-      }
+          KEYFRAMES[name].Y = y;
+        }
+      };
+      const toDegrees = (x, y) => {
+        let retVal = Math.atan2(y, x) * 180 / Math.PI + 90;
 
-      return retVal;
-    }
+        if (x < 0 && y < 0) {
+          retVal += 360;
+        }
 
-    animation.setSubframe(SUBFRAME);
-    animation.setSpeed(SPEED);
+        return retVal;
+      };
+      const addDotsKeyframe = (name, key, x, y) => {
+        initData(name, x, y);
 
-    animation.onEnterFrame = event => {
-      const elements = animation.renderer.elements;
+        x -= KEYFRAMES[name].X;
+        y -= KEYFRAMES[name].Y;
 
-      elements.forEach(element => {
-        const name = element.data.nm.toLowerCase();
+        if (x !== 0 || y !== 0) {
+          x = Math.round(x);
+          y = Math.round(y);
 
-        if (animation.animationData.nm.toLowerCase() === 'spinner') {
-          const $svg = $(animation.wrapper);
-          const $path = $svg.find('path');
-          const path = $path.attr('d');
+          if (y === 0) {
+            KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px); }`;
+          } else {
+            KEYFRAMES[name][key] = `${key}% { transform: translate(${x}px, ${y}px); }`;
+          }
+        }
+      };
+      const addSpinnerKeyframe = (key, x, y, length) => {
+        const name = 'spinner';
+        const degrees = toDegrees(x, y);
+        const rotate = Math.round(degrees - 90); // compensate for stroke start point
 
-          if (path) {
-            const matches = path.match(PATTERN_PATH);
+        initData(name, 0, 0);
+        length = Math.round(length / 7.5); // downsize from lottie
+        KEYFRAMES[name][key] = `${key}% { transform: rotate(${rotate}deg); stroke-dasharray: ${length} 250; }`;
+      };
 
-            if (matches) {
-              const [x, y] = matches.slice(1);
-              const degrees = toDegrees(x, y);
-              const radius = $svg.width() / 2;
-              const offset = 2 * Math.PI * radius * (degrees / 360);
+      animation.setSubframe(SUBFRAME);
+      animation.setSpeed(SPEED);
 
-              addDash(name, event.currentTime, degrees, $path[0].getTotalLength(), x, y);
+      animation.onEnterFrame = event => {
+        const elements = animation.renderer.elements;
+
+        elements.forEach(element => {
+          const name = element.data.nm.toLowerCase();
+
+          if (animation.animationData.nm.toLowerCase() === 'spinner') {
+            const $svg = $(animation.wrapper);
+            const $path = $svg.find('path');
+            const path = $path.attr('d') || '';
+            const point = path.match(PATTERN_STARTPOINT);
+
+            if (point) {
+              const [x, y] = point.slice(1);
+              const length = $path[0].getTotalLength();
+
+              addSpinnerKeyframe(event.currentTime, parseFloat(x), parseFloat(y), length);
+            }
+          } else {
+            const matrix = element.finalTransform.mat.to2dCSS().match(PATTERN_MATRIX);
+
+            if (matrix) {
+              const [x, y] = matrix.slice(1);
+
+              addDotsKeyframe(name, event.currentTime, parseFloat(x), parseFloat(y));
             }
           }
-        } else {
-          const matrix = element.finalTransform.mat.to2dCSS();
-          const [x, y] = matrix.match(PATTERN_MATRIX).slice(1);
-
-          addTransform(name, event.currentTime, parseFloat(x), parseFloat(y));
-        }
-      });
-    }
-
-    animation.onLoopComplete = (event) => {
-      if (event.currentLoop === 1) {
-        Object.keys(KEYFRAMES).forEach(name => {
-          let keyframes = `@keyframes zd-${name} {`;
-          let keys = Object.keys(KEYFRAMES[name]).filter(key => (key !== 'X' && key !== 'Y'));
-
-          keys.forEach(key => {
-            keyframes = `${keyframes}\n  ${KEYFRAMES[name][key]}`;
-          });
-
-          keyframes = `${keyframes}\n}`;
-          console.log(keyframes);
         });
       }
-    }
 
-    animation.addEventListener('DOMLoaded', function() {
-      $('.js-animate').triggerHandler('click');
+      animation.onLoopComplete = (event) => {
+        if (event.currentLoop === 1) {
+          Object.keys(KEYFRAMES).forEach(name => {
+            let keyframes = `@keyframes zd-${name} {`;
+            let keys = Object.keys(KEYFRAMES[name]).filter(key => (key !== 'X' && key !== 'Y'));
+
+            keys.forEach(key => {
+              keyframes = `${keyframes}\n  ${KEYFRAMES[name][key]}`;
+            });
+
+            keyframes = `${keyframes}\n}`;
+            console.log(keyframes);
+          });
+        }
+      }
+
+      animation.addEventListener('DOMLoaded', function() {
+        $('.js-animate').triggerHandler('click');
+      });
     });
   } else {
     $('.js-animate').triggerHandler('click');
