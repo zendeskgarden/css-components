3d94a2a457e494e6b12e69f9f921de64358891d8
Jonathan Zempel <jzempel@gmail.com>
2018-09-04 15:39:20 -0700

    Update component styling

--- demo/scrollbars/index.js
+++ demo/scrollbars/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-  Garden.customClasses.push('.c-scrollbars');
-  Garden.rtlClasses.push('.c-scrollbars');
+  Garden.customClasses.push('.c-scrollbar');
+  Garden.rtlClasses.push('.c-scrollbar');
+
+  new PerfectScrollbar('.c-scrollbar');
+
+  var scrollbar = null;
+
+  $('.c-playground').on('change', function() {
+    if (scrollbar) {
+      scrollbar.destroy();
+      scrollbar = null;
+    }
+
+    scrollbar = new PerfectScrollbar('.c-playground .c-scrollbar');
+  });
 });
