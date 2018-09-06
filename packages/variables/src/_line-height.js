14c3767c3375689efe326e063d782ec3e22b1a4d
Jonathan Zempel <jzempel@gmail.com>
2018-09-06 15:52:21 -0700

    Add line-height utilities

---
+++ packages/variables/src/_line-height.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+const retVal = {
+  sm: '16px',
+  md: '20px',
+  lg: '24px',
+  xl: '28px',
+  xxl: '32px',
+  xxxl: '44px'
+};
+
+module.exports = retVal;
