323c4598f42bff2747d3dd8c3ae292254394d405
Jonathan Zempel <jzempel@gmail.com>
2018-09-12 15:01:16 -0700

    Fill out demo; add dark mode styling

--- demo/scrollbars/index.js
+++ demo/scrollbars/index.js
@@ -1,17 +1,44 @@
 $(document).ready(function() {
-  Garden.customClasses.push('.c-scrollbar');
   Garden.rtlClasses.push('.c-scrollbar');
 
-  new PerfectScrollbar('.c-scrollbar');
+  $('.c-scrollbar:not(.js-not)').each(function() {
+    var scrollbar = new PerfectScrollbar($(this)[0]);
+
+    $(this).data('scrollbar', scrollbar);
+  });
 
   var scrollbar = null;
 
   $('.c-playground').on('change', function() {
-    if (scrollbar) {
-      scrollbar.destroy();
-      scrollbar = null;
-    }
+    $('.c-playground .c-scrollbar').each(function() {
+      var scrollbar = $(this).data('scrollbar');
 
-    scrollbar = new PerfectScrollbar('.c-playground .c-scrollbar');
+      if (scrollbar) {
+        scrollbar.destroy();
+        scrollbar = null;
+      }
+
+      new PerfectScrollbar($(this)[0]);
+    });
+  });
+
+  $('.js-rtl').click(function() {
+    $('.c-scrollbar:not(.js-not):not(.c-playground .c-scrollbar)').each(function () {
+      $(this).data('scrollbar').destroy();
+      $(this).data('scrollbar', null);
+      new PerfectScrollbar($(this)[0]);
+    });
+  });
+
+  $('.js-size').on('change input', function() {
+    var $this = $(this);
+    var $scrollbar = $this.parent().siblings('.c-scrollbar');
+    var $img = $scrollbar.children('img');
+    var width = ($this.val() / 100) * 2560;
+
+    $img.width(width);
+    $scrollbar.data('scrollbar').update();
+  }).click(function() {
+    return false;
   });
 });
