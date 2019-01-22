666e51068a194dae597a252d8faf7804d4003027
Jonathan Zempel <jzempel@gmail.com>
2019-01-22 12:58:48 -0800

    chore: remove deprecated colors and font sizes (#169)
    BREAKING CHANGE: remove long-deprecated color names and font sizes from `css-variables` and `css-utilities`.

--- packages/variables/src/_font-size.js
+++ packages/variables/src/_font-size.js
@@ -5,7 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-let retVal = {
+const retVal = {
   sm: '12px',
   md: '14px',
   lg: '18px',
@@ -13,26 +13,5 @@ let retVal = {
   xxl: '26px',
   xxxl: '36px'
 };
-
-/* Deprecated sizes */
-retVal = Object.assign(retVal, {
-  /* Larger than H1 */
-  giga: retVal['xxxl'],
-  mega: '28px',
-  kilo: '25px',
-
-  /* H1-H6 */
-  alpha: retVal['xl'],
-  beta: '20px',
-  gamma: retVal['lg'],
-  delta: '16px',
-  epsilon: retVal['md'],
-  zeta: retVal['sm'],
-
-  /* Smaller than H6 */
-  milli: '11px',
-  micro: '10px',
-  nano: '9px'
-});
 
 module.exports = retVal;
