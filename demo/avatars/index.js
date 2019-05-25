f2d97d28bb254e83f188da2f1dff42782a9eb19a
Jonathan Zempel <jzempel@gmail.com>
2019-05-25 16:17:44 -0700

    Initial "presence" styling

--- demo/avatars/index.js
+++ demo/avatars/index.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+  Garden.rtlClasses.push('.c-avatar');
+
+  $('.c-avatar:not(.c-avatar--system) img').each(function() {
+    var source = 'images/avatar-' + Math.floor(Math.random() * 20 + 1) + '.png';
+
+    $(this).attr('src', source);
+  });
+
   $('.js-dark').click(function() {
     var state = $('body').hasClass('is-dark');
+  });
 
-    $('.c-avatar:not(.c-playground .c-avatar)').toggleClass('c-avatar--borderless', state);
+  $('.js-status').change(function() {
+    var status = $(this).val();
+
+    $('.c-avatar:not(.c-playground .c-avatar)').removeClass('is-online is-away is-active');
+
+    if (status.length > 0) {
+      $('.c-avatar:not(.c-playground .c-avatar)').addClass('is-' + status);
+    }
   });
 });
