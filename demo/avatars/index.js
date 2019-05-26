f40d1f61d7ab32f27cb5a6cdf1ba0245de6afee7
Jonathan Zempel <jzempel@gmail.com>
2019-05-25 22:06:54 -0700

    Badge styling for active state

--- demo/avatars/index.js
+++ demo/avatars/index.js
@@ -11,6 +11,21 @@ $(document).ready(function() {
     var state = $('body').hasClass('is-dark');
   });
 
+  $('.js-count')
+    .on('change input', function() {
+      var count = $(this).val();
+      var data = count > 0 ? count : '';
+
+      if (count > 9) {
+        data = '9+';
+      }
+
+      $('.c-avatar:not(.c-playground .c-avatar)').attr('data-badge', data);
+    })
+    .click(function() {
+      return false;
+    });
+
   $('.js-status').change(function() {
     var status = $(this).val();
 
@@ -19,5 +34,7 @@ $(document).ready(function() {
     if (status.length > 0) {
       $('.c-avatar:not(.c-playground .c-avatar)').addClass('is-' + status);
     }
+
+    $('.js-count').prop('disabled', status !== 'active');
   });
 });
