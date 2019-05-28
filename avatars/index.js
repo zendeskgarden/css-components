687252cffd5dc414eba5ed38eb897e6fdcaf124b
Jonathan Zempel <jzempel@gmail.com>
2019-05-27 19:27:33 -0700

    Updates

--- avatars/index.js
+++ avatars/index.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
+  Garden.rtlClasses.push('.c-avatar');
+
+  $('.c-avatar:not(.c-avatar--system) img').each(function() {
+    var source = 'images/avatar-' + Math.floor(Math.random() * 20 + 1) + '.png';
+
+    $(this).attr('src', source);
+  });
+
   $('.js-dark').click(function() {
     var state = $('body').hasClass('is-dark');
+  });
 
-    $('.c-avatar:not(.c-playground .c-avatar)').toggleClass('c-avatar--borderless', state);
+  $('.js-count')
+    .on('change input', function() {
+      var count = $(this).val();
+      var text = count > 0 ? count : '';
+
+      if (count > 9) {
+        text = '9+';
+      }
+
+      $('.c-avatar:not(.c-playground .c-avatar) .c-avatar__badge').text(text);
+    })
+    .click(function() {
+      return false;
+    });
+
+  $('.js-status').change(function() {
+    var status = $(this).val();
+
+    $('.c-avatar:not(.c-playground .c-avatar)').removeClass('is-online is-away is-active');
+
+    if (status.length > 0) {
+      $('.c-avatar:not(.c-playground .c-avatar)').addClass('is-' + status);
+      $('.js-count').change();
+    }
+
+    $('.js-count').prop('disabled', status !== 'active');
   });
 });
