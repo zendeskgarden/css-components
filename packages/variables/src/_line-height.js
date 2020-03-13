17adbbfd4ccd8d6f0e21f2f92fe1d3002c7e9e9f
Jonathan Zempel <jzempel@gmail.com>
2020-03-13 16:52:06 -0700

    Get line heights and spacing from `react-theming`

--- packages/variables/src/_line-height.js
+++ packages/variables/src/_line-height.js
@@ -5,13 +5,6 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-const retVal = {
-  sm: '16px',
-  md: '20px',
-  lg: '24px',
-  xl: '28px',
-  xxl: '32px',
-  xxxl: '44px'
-};
+const lineHeights = require('@zendeskgarden/react-theming').DEFAULT_THEME.lineHeights;
 
-module.exports = retVal;
+module.exports = { ...lineHeights };
