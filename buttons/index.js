a2fd71fa9a292a46cab5f75316b6ff7b2409c819
Zendesk Garden <garden@zendesk.com>
2020-05-11 22:17:13 +0000

    Updates [skip ci]

--- buttons/index.js
+++ buttons/index.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  Garden.customClasses.push('.c-btn');
-  Garden.smClasses.push('.c-btn');
-  Garden.rtlClasses.push('.l-btn-group');
-  Garden.handleFocus('.c-btn');
+  Garden.rtlClasses.push('.c-btn');
 
   $('.js-inset').change(function() {
     $('.c-btn').toggleClass('c-btn--focus-inset');
   });
 
-  $(document).on('click', '.l-btn-group[role=tablist] .c-btn:not(:disabled):not(.is-disabled)', function() {
-    $(this).addClass('is-selected').siblings('.c-btn').removeClass('is-selected');
-  });
+  $('.js-rotate').click(function() {
+    $(this).toggleClass('is-active').children('.c-btn__icon').toggleClass('is-rotated');
+  })
 });
