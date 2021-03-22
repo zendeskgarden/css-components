62d2652e1802d839157a69127116c912d3700a69
Zendesk Garden <garden@zendesk.com>
2021-03-22 18:04:12 +0000

    Updates [skip ci]

--- callouts/index.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  Garden.darkClasses.push('.c-callout');
-  Garden.rtlClasses.push('.c-callout');
-});
--- pagination/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  Garden.customClasses.push('.c-pagination');
-  Garden.rtlClasses.push('.c-pagination');
-  Garden.handleFocus('.c-pagination__page');
-});
