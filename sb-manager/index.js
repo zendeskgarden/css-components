32de45c3aea4a98004a08d75c44922ea200e5bd0
Zendesk Garden <garden@zendesk.com>
2024-06-12 19:40:51 +0000

    Updates [skip ci]

--- sb-manager/index.js
+++ sb-manager/index.js
@@ -1,1 +1,1 @@
-import{Provider,Root,renderStorybookUI}from"./chunk-O6NCVXC4.js";import"./chunk-RHK3WMPG.js";import"./chunk-TZAR34JC.js";import"./chunk-VMGB76WP.js";import"./chunk-UOBNU442.js";import"./chunk-XP3HGWTR.js";export{Provider,Root,renderStorybookUI};
+import{Provider,Root,renderStorybookUI}from"./chunk-KQ7SHZUB.js";import"./chunk-KB6ESMRN.js";import"./chunk-GUVK2GTO.js";import"./chunk-BLWCBWKL.js";import"./chunk-B3YDJJJH.js";import"./chunk-ZR5JZWHI.js";export{Provider,Root,renderStorybookUI};
