b243a0b75a48ed2febc2572431a129c5e325e5f7
Zendesk Garden <garden@zendesk.com>
2024-08-12 14:06:49 +0000

    Updates [skip ci]

--- sb-manager/index.js
+++
@@ -1,1 +0,0 @@
-import{Provider,Root,renderStorybookUI}from"./chunk-KQ7SHZUB.js";import"./chunk-KB6ESMRN.js";import"./chunk-GUVK2GTO.js";import"./chunk-BLWCBWKL.js";import"./chunk-B3YDJJJH.js";import"./chunk-ZR5JZWHI.js";export{Provider,Root,renderStorybookUI};
