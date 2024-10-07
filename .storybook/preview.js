e8461a83904c3ba05ff1e0585f22ee59f6467bb4
Jonathan Zempel <jzempel@gmail.com>
2024-10-07 10:19:07 -0400

    Configure dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,8 +8,42 @@
 import '@zendeskgarden/css-grid';
 import '@zendeskgarden/css-utilities';
 import './index.css';
-import { DEFAULT_THEME } from '@zendeskgarden/react-theming';
+import { DEFAULT_THEME, ThemeProvider, getColor } from '@zendeskgarden/react-theming';
+import React from 'react';
 import { create } from '@storybook/theming/create';
+import { createGlobalStyle } from 'styled-components';
+
+const DARK_THEME = { ...DEFAULT_THEME, colors: { ...DEFAULT_THEME.colors, base: 'dark' } };
+const DARK = getColor({ theme: DARK_THEME, variable: 'background.default' });
+const LIGHT = getColor({ theme: DEFAULT_THEME, variable: 'background.default' });
+
+export const parameters = {
+  backgrounds: {
+    grid: { disable: true },
+    values: [
+      { name: 'light', value: LIGHT },
+      { name: 'dark', value: DARK }
+    ]
+  },
+  controls: { hideNoControlsWarning: true },
+  docs: {
+    theme: create({
+      base: DEFAULT_THEME.colors.base
+    })
+  },
+  options: {
+    storySort: {
+      order: ['README']
+    }
+  }
+};
+
+const GlobalPreviewStyling = createGlobalStyle`
+  html {
+    background-color: ${p => getColor({ theme: p.theme, variable: 'background.default' })};
+    color: ${p => getColor({ theme: p.theme, variable: 'foreground.default' })};
+  }
+`;
 
 export const decorators = [
   (story, context) => {
@@ -19,10 +53,32 @@ export const decorators = [
       document.querySelector('link[href="index.css"]').setAttribute('disabled', true);
     }
 
-    /* eslint-disable-next-line n/global-require */
-    require('focus-visible');
+    const colors = {
+      ...DEFAULT_THEME.colors,
+      primaryHue: context.globals.primaryHue,
+      variables: {
+        ...DEFAULT_THEME.colors.variables,
+        dark: context.args['colors.dark'],
+        light: context.args['colors.light']
+      }
+    };
 
-    return story();
+    if (
+      context.globals.backgrounds && context.globals.backgrounds.value !== 'transparent'
+        ? context.globals.backgrounds.value === DARK
+        : context.parameters.backgrounds.default === 'dark'
+    ) {
+      colors.base = 'dark';
+    }
+
+    const theme = { ...DEFAULT_THEME, colors };
+
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalPreviewStyling />
+        {story()}
+      </ThemeProvider>
+    );
   }
 ];
 
@@ -37,24 +93,6 @@ export const globalTypes = {
         { value: 'enabled', title: 'Bedrock enabled' },
         { value: 'disabled', title: 'Bedrock disabled' }
       ]
-    }
-  }
-};
-
-export const parameters = {
-  backgrounds: {
-    default: DEFAULT_THEME.colors.base,
-    grid: { disable: true }
-  },
-  controls: { hideNoControlsWarning: true },
-  docs: {
-    theme: create({
-      base: DEFAULT_THEME.colors.base
-    })
-  },
-  options: {
-    storySort: {
-      order: ['README']
     }
   }
 };
