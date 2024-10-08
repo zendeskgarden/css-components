d448e396dc78e9dc38f2324043ff0c64f89c4b91
Jonathan Zempel <jzempel@gmail.com>
2024-10-08 13:04:15 -0400

    Add dark styling variant

--- packages/buttons/demo/buttons.stories.js
+++ packages/buttons/demo/buttons.stories.js
@@ -8,13 +8,14 @@
 import '../dist/index.css';
 import { Grid } from '@zendeskgarden/react-grid';
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 export default {
   title: 'Components/Buttons'
 };
 
-const toClassName = (base, size = false, style = false) => {
-  let retVal = base;
+const toClassName = (theme, base, size = false, style = false) => {
+  let retVal = theme.colors.base === 'dark' ? `${base} c-btn--dark` : base;
 
   if (size) {
     retVal += ` ${size}`;
@@ -27,8 +28,8 @@ const toClassName = (base, size = false, style = false) => {
   return retVal;
 };
 
-const toClassSelector = (base, size = false, style = false) => {
-  let retVal = base;
+const toClassSelector = (theme, base, size = false, style = false) => {
+  let retVal = theme.colors.base === 'dark' ? `${base}.c-btn--dark` : base;
 
   if (size) {
     retVal += `.${size}`;
@@ -42,35 +43,44 @@ const toClassSelector = (base, size = false, style = false) => {
 };
 
 export const Types = {
-  render: ({ disabled, size = false, style = false }) => (
-    <Grid>
-      <Grid.Row>
-        <Grid.Col className="mt-5">
-          <button type="button" className={toClassName('c-btn', size, style)} disabled={disabled}>
-            {toClassSelector('.c-btn', size, style)}
-          </button>
-        </Grid.Col>
-        <Grid.Col className="mt-5">
-          <button
-            type="button"
-            className={toClassName('c-btn c-btn--pill', size, style)}
-            disabled={disabled}
-          >
-            {toClassSelector('.c-btn.c-btn--pill', size, style)}
-          </button>
-        </Grid.Col>
-        <Grid.Col className="mt-5">
-          <button
-            type="button"
-            className={toClassName('c-btn c-btn--basic', size, style)}
-            disabled={disabled}
-          >
-            {toClassSelector('.c-btn.c-btn--basic', size, style)}
-          </button>
-        </Grid.Col>
-      </Grid.Row>
-    </Grid>
-  ),
+  render: ({ disabled, size = false, style = false }) => {
+    /* eslint-disable-next-line react-hooks/rules-of-hooks */
+    const theme = useTheme();
+
+    return (
+      <Grid>
+        <Grid.Row>
+          <Grid.Col className="mt-5">
+            <button
+              type="button"
+              className={toClassName(theme, 'c-btn', size, style)}
+              disabled={disabled}
+            >
+              {toClassSelector(theme, '.c-btn', size, style)}
+            </button>
+          </Grid.Col>
+          <Grid.Col className="mt-5">
+            <button
+              type="button"
+              className={toClassName(theme, 'c-btn c-btn--pill', size, style)}
+              disabled={disabled}
+            >
+              {toClassSelector(theme, '.c-btn.c-btn--pill', size, style)}
+            </button>
+          </Grid.Col>
+          <Grid.Col className="mt-5">
+            <button
+              type="button"
+              className={toClassName(theme, 'c-btn c-btn--basic', size, style)}
+              disabled={disabled}
+            >
+              {toClassSelector(theme, '.c-btn.c-btn--basic', size, style)}
+            </button>
+          </Grid.Col>
+        </Grid.Row>
+      </Grid>
+    );
+  },
   name: 'Types',
   argTypes: {
     size: {
@@ -91,58 +101,64 @@ export const Types = {
         type: 'boolean'
       }
     }
-  }
+  },
+  decorators: [Story => <Story />]
 };
 
 export const Icons = {
-  render: ({ disabled, size, style }) => (
-    <Grid>
-      <Grid.Row>
-        <Grid.Col className="mt-5">
-          <button
-            type="button"
-            className={toClassName('c-btn c-btn--icon', size, style)}
-            disabled={disabled}
-          >
-            <svg
-              className="c-btn__icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              focusable="false"
-              viewBox="0 0 26 26"
+  render: ({ disabled, size, style }) => {
+    /* eslint-disable-next-line react-hooks/rules-of-hooks */
+    const theme = useTheme();
+
+    return (
+      <Grid>
+        <Grid.Row>
+          <Grid.Col className="mt-5">
+            <button
+              type="button"
+              className={toClassName(theme, 'c-btn c-btn--icon', size, style)}
+              disabled={disabled}
             >
-              <path
-                fill="currentColor"
-                d="M12 8.2v14.5H0zM12 3c0 3.3-2.7 6-6 6S0 6.3 0 3h12zm2 19.7c0-3.3 2.7-6 6-6s6 2.7 6 6H14zm0-5.2V3h12z"
-              />
-            </svg>
-          </button>
-        </Grid.Col>
-        <Grid.Col className="mt-5">
-          <button
-            type="button"
-            className={toClassName('c-btn c-btn--icon', size, style)}
-            disabled={disabled}
-          >
-            <svg
-              className="c-btn__icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              focusable="false"
-              viewBox="0 0 16 16"
+              <svg
+                className="c-btn__icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                focusable="false"
+                viewBox="0 0 26 26"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 8.2v14.5H0zM12 3c0 3.3-2.7 6-6 6S0 6.3 0 3h12zm2 19.7c0-3.3 2.7-6 6-6s6 2.7 6 6H14zm0-5.2V3h12z"
+                />
+              </svg>
+            </button>
+          </Grid.Col>
+          <Grid.Col className="mt-5">
+            <button
+              type="button"
+              className={toClassName(theme, 'c-btn c-btn--icon', size, style)}
+              disabled={disabled}
             >
-              <path
-                fill="currentColor"
-                d="M12.688 5.61a.5.5 0 0 1 .69.718l-.066.062-5 4a.5.5 0 0 1-.542.054l-.082-.054-5-4a.5.5 0 0 1 .55-.83l.074.05L8 9.359l4.688-3.75z"
-              />
-            </svg>
-          </button>
-        </Grid.Col>
-      </Grid.Row>
-    </Grid>
-  ),
+              <svg
+                className="c-btn__icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                focusable="false"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12.688 5.61a.5.5 0 0 1 .69.718l-.066.062-5 4a.5.5 0 0 1-.542.054l-.082-.054-5-4a.5.5 0 0 1 .55-.83l.074.05L8 9.359l4.688-3.75z"
+                />
+              </svg>
+            </button>
+          </Grid.Col>
+        </Grid.Row>
+      </Grid>
+    );
+  },
   name: 'Icons',
   argTypes: {
     size: {
@@ -163,5 +179,6 @@ export const Icons = {
         type: 'boolean'
       }
     }
-  }
+  },
+  decorators: [Story => <Story />]
 };
